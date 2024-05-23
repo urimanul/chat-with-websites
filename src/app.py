96d6dfa1358f1f30b756d341bcee70fed60b1866
urimanul <myopera9@gmail.com>
2024-05-24 08:59:17 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,18 +1,44 @@
 # pip install streamlit langchain lanchain-openai beautifulsoup4 python-dotenv chromadb
 
 import streamlit as st
+import mysql.connector as mydb
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+#from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
+# コネクションの作成
+conn = mydb.connect(
+    host='www.ryhintl.com',
+    port='36000',
+    user='smairuser',
+    password='smairuser',
+    database='smair'
+)
 
-load_dotenv()
+cur = conn.cursor()
+cur.execute("SELECT * FROM openai_payload")
+
+# 全てのデータを取得
+rows = cur.fetchall()
+    
+# Get api_key
+for (spoid, key_name, api_key) in rows:
+        OPENAI_API_KEY=f"{api_key}"
+        #api_key = f"{api_key}"
+        #print(api_key)
+    
+cur.close()
+conn.close()
+
+#load_dotenv()
+
 
 def get_vectorstore_from_url(url):
     # get the text in document form
@@ -24,12 +50,13 @@ def get_vectorstore_from_url(url):
     document_chunks = text_splitter.split_documents(document)
     
     # create a vectorstore from the chunks
-    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+    #vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+    vector_store = FAISS.from_documents(document_chunks, OpenAIEmbeddings())
 
     return vector_store
 
 def get_context_retriever_chain(vector_store):
-    llm = ChatOpenAI()
+    llm = ChatOpenAI(openai_api_key=f"{OPENAI_API_KEY}")
     
     retriever = vector_store.as_retriever()
     
@@ -74,23 +101,23 @@ st.title("Chat with websites")
 
 # sidebar
 with st.sidebar:
-    st.header("Settings")
-    website_url = st.text_input("Website URL")
+    st.header("設定")
+    website_url = st.text_input("ウェブサイト URL")
 
 if website_url is None or website_url == "":
-    st.info("Please enter a website URL")
+    st.info("ウェブサイトの URL を入力してください")
 
 else:
     # session state
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = [
-            AIMessage(content="Hello, I am a bot. How can I help you?"),
+            AIMessage(content="こんにちは、ボットです。どんな御用でしょうか？"),
         ]
     if "vector_store" not in st.session_state:
         st.session_state.vector_store = get_vectorstore_from_url(website_url)    
 
     # user input
-    user_query = st.chat_input("Type your message here...")
+    user_query = st.chat_input("ここにメッセージを入力してください...")
     if user_query is not None and user_query != "":
         response = get_response(user_query)
         st.session_state.chat_history.append(HumanMessage(content=user_query))
@@ -104,5 +131,5 @@ else:
             with st.chat_message("AI"):
                 st.write(message.content)
         elif isinstance(message, HumanMessage):
-            with st.chat_message("Human"):
+            with st.chat_message("人間"):
                 st.write(message.content)
